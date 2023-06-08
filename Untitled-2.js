075ec3ea50c6eed84825a9a93b376be07fcb17b5
Ayaz Aliyev <119433345+AyazAliyv@users.noreply.github.com>
2023-06-08 20:28:43 +0100

    Delete Untitled-2.js

--- Untitled-2.js
+++
@@ -1,6 +0,0 @@
-fetch('https://api.github.com/users/<AyazAliyv>/repos')
-    .then(response => response.json())
-    .then(data => {
-        // Do something with your data
-        console.log(data);
-    });
